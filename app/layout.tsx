4f46f8611fbc377041ed907220a7a9addb21f0bd
gymmeg123 <gymmeg325@gmail.com>
2025-06-29 18:34:23 +0530

    Update site title to meggym

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'meggym',
+  title: 'MEG GYM',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
